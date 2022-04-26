ec9f1dfbdd8dcd39035ac781fcd1c3105f98471c
Jiwidi <fhjaime96@gmail.com>
2022-04-26 15:28:28 +0200

    Some progress for features in cf

--- torchrecsys/datasets/datasets.py
+++ torchrecsys/datasets/datasets.py
@@ -2,63 +2,78 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
+
+##TODO
+class feature:
+    name: str
+    dtype: str
+    unique_values: str
+
+
 def dataframe_schema(df):
     r = []
     for col in df.columns.values:
         col_schema = [df[col].dtype.name, len(df[col].unique())]
         r.append(col_schema)
-        
+
     return r
 
+
 class InteractionsDataset(torch.utils.data.Dataset):
-    def __init__(self, interactions, user_features,  item_features, sample_negatives=0, target_column=None):
-        
+    def __init__(
+        self,
+        interactions,
+        user_features,
+        item_features,
+        sample_negatives=0,
+        target_column=None,
+    ):
+
         if not target_column:
             interactions = interactions[interactions.columns[:2]]
             context_features = interactions[interactions.columns[2:]]
         else:
             interactions = interactions[interactions.columns[:3]]
             context_features = interactions[interactions.columns[3:]]
-            
-        
-        self.interactions = interactions.values #First two columns: userid, itemid
+
+        self.interactions = interactions.values  # First two columns: userid, itemid
         self.context_features = context_features.values
-        
+
         self.user_features = dict(zip(user_features.user, user_features.values))
         self.item_features = dict(zip(item_features.item, item_features.values))
-        
+
         if target_column and sample_negatives:
-            assert 1==0 #Error because logic wont work
-            
-        #Create a nice way of loading context + item features into a single dataset. Generate schema that models read from and are able to create 
+            assert 1 == 0  # Error because logic wont work
+
+        # Create a nice way of loading context + item features into a single dataset. Generate schema that models read from and are able to create
         self.n_users = user_features.user.max()
         self.n_items = item_features.item.max()
-        
+
         self.interactions_pd_schema = dataframe_schema(interactions)
         self.context_pd_schema = dataframe_schema(context_features)
         self.item_pd_schema = dataframe_schema(item_features)
         self.user_pd_schema = dataframe_schema(user_features)
-        
+
         self.target_column = target_column
-        
-        #To do add custom target column
-        
+
+        # To do add custom target column
+
     def __len__(self):
         return len(self.interactions)
 
     def __getitem__(self, idx):
         interaction = self.interactions[idx]
         context_features = self.context_features[idx]
-        
+
         user_features = self.user_features[interaction[0]]
         item_features = self.item_features[interaction[1]]
-        
+
         return interaction, context_features, user_features, item_features
-    
+
     def __castdtypes(self, data):
         """Ensure needed dtypes for the data_Schema"""
         pass
-    
+
     @property
     def data_schema(self):
         return {
@@ -66,34 +81,40 @@ class InteractionsDataset(torch.utils.data.Dataset):
             "context": self.context_pd_schema,
             "user_features": self.user_pd_schema,
             "item_features": self.item_pd_schema,
-            "objetive": "notbinary?"
+            "objetive": "notbinary?",
         }
-    
+
     @staticmethod
     def collate_fn(batch_output):
         return batch_output
-        
 
-    
-    
-    
+
 class SequenceDataset(torch.utils.data.Dataset):
-    def __init__(self, dataframe, identifier_column=0, interaction_column=1, sample_negatives = False, negatives_per_positive=0, max_len=10, training=True):
+    def __init__(
+        self,
+        dataframe,
+        identifier_column=0,
+        interaction_column=1,
+        sample_negatives=False,
+        negatives_per_positive=0,
+        max_len=10,
+        training=True,
+    ):
         self.data = dataframe
         self.identifier_column = identifier_column
         self.interaction_column = interaction_column
         self.max_len = max_len
         self.training = training
-    
+
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         sequence = self.data[idx][self.identifier_column]
-        
-        sequence = torch.LongTensor(sequence[-self.max_len:])
-        sequence = F.pad(sequence, (self.max_len - sequence.shape[0],0))
-        
+
+        sequence = torch.LongTensor(sequence[-self.max_len :])
+        sequence = F.pad(sequence, (self.max_len - sequence.shape[0], 0))
+
         if self.training:
             interaction = self.data[idx][self.interaction_column]
             return torch.LongTensor(sequence), interaction
--- torchrecsys/models/neuralcf.py
+++ torchrecsys/models/neuralcf.py
@@ -7,73 +7,102 @@ import numpy as np
 import torch
 from torch import nn
 
+
 class NeuralCF(BaseModel):
-    def __init__(self, data_schema, lr_rate=0.01, embedding_size=64, layers=[512], ):
+    def __init__(
+        self,
+        data_schema,
+        lr_rate=0.01,
+        embedding_size=64,
+        feature_embedding_size=8,
+        layers=[512],
+    ):
         super().__init__()
         interactions_schema = data_schema["interactions"]
-        
+
         self.n_users = interactions_schema[0]
         self.n_items = interactions_schema[1]
-        
-        self.user_embedding = nn.Embedding(self.n_users+1, embedding_size)
-        self.item_embedding = nn.Embedding(self.n_items+1, embedding_size)
-        
-        
-        
+
+        self.user_embedding = nn.Embedding(self.n_users + 1, embedding_size)
+        self.item_embedding = nn.Embedding(self.n_items + 1, embedding_size)
+
         self.linear = nn.Linear(embedding_size, layers[0])
-        
+
         self.final_linear = nn.Linear(layers[0], 1)
-        self.final_activation = torch.sigmoid if data_schema["objetive"]=="binary" else lambda x: x
-        
-        
-        
-        self.criterion = torch.nn.BCELoss() if data_schema["objetive"]=="binary" else torch.nn.MSELoss()
-        
+
+        # User features encoding
+
+        # Item features encoding
+        self.item_feature_types = []
+        for feature_idx, feature in enumerate(data_schema["item_features"]):
+            feature_type, value_count = feature
+            if feature_type == "category":
+                layer_name = f"feature{feature_idx}_embedding"
+                setattr(
+                    self,
+                    layer_name,
+                    nn.Embedding(value_count + 1, feature_embedding_size),
+                )
+                self.item_feature_types.append([feature[0], layer_name, feature_idx])
+
+        self.criterion = (
+            torch.nn.BCEWithLogitsLoss()
+            if data_schema["objetive"] == "binary"
+            else torch.nn.MSELoss()
+        )
+
         self.lr_rate = lr_rate
 
-    def forward(self, interactions):
-        
+    def forward(self, interactions, context, users, items):
+
         user = self.user_embedding(interactions[:, 0].long())
         item = self.item_embedding(interactions[:, 1].long())
-        
-        x = (user * item)
+
+        aux = self.encode_item(items)
+        x = user * item
         x = self.linear(x)
         x = self.final_linear(x)
-        x = self.final_activation(x)
-        
 
         return x
-    
+
+    def encode_user(self, user):
+        return user
+
+    def encode_item(self, items):
+        r = []
+        for idx, feature in enumerate(self.item_feature_types):
+            feature_type, layer_name, feature_idx = feature
+            if feature_type == "category":
+                feature_embedding = getattr(self, layer_name)(items[:, feature_idx])
+                r.append(feature_embedding)
+        r = torch.cat(r)
+        return r
+
     def training_step(self, batch):
-        interactions, context, items, users = batch
-        
-        yhat = self(interactions).float()
+        yhat = self(*batch).float()
         yhat = torch.squeeze(yhat)
-        
-        ytrue = interactions[:, 2].float()
-        
+
+        ytrue = batch[0][:, 2].float()
+
         loss = self.criterion(yhat, ytrue)
-        
+
         self.log("train/step_loss", loss, on_step=True, on_epoch=False, prog_bar=False)
-    
+
         return loss
-        
-        
+
     def validation_step(self, batch):
-        interactions, context, items, users = batch
-        
-        yhat = self(interactions)
-        ytrue = torch.squeeze(interactions[:, 2])
+        yhat = self(*batch).float()
+        ytrue = batch[0][:, 2].float()
         loss = self.criterion(yhat, ytrue)
-        
-        self.log("validation/step_loss", loss, on_step=True, on_epoch=False, prog_bar=False)
-        
-        
+
+        self.log(
+            "validation/step_loss", loss, on_step=True, on_epoch=False, prog_bar=False
+        )
+
         return loss
-    
+
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), self.lr_rate)
-        
+
         return optimizer
-    
- 
+
