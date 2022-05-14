cefa69bd8577d28c90f1c6347142f6b3b7e2a9aa
jiwidi <fhjaime96@gmail.com>
2022-05-14 19:44:38 +0200

    Bert4rec feature creation update

--- torchrecsys/models/bert4rec.py
+++ torchrecsys/models/bert4rec.py
@@ -2,8 +2,8 @@ import pytorch_lightning as pl
 import torch
 from torch import nn
 
-from torchrecsys.layers import CategoricalLayer, NumericalLayer
 from torchrecsys.models.bert_utils import BERTEmbedding, TransformerBlock
+from torchrecsys.models.utils import schema_to_featureModuleList
 
 
 class Bert4Rec(pl.LightningModule):
@@ -36,27 +36,9 @@ class Bert4Rec(pl.LightningModule):
         self.num_rec = num_rec
 
         # Item features encoding
-        self.item_features = nn.ModuleList()
-        self.item_feature_dimension = 0
-        for feature_idx, feature in enumerate(data_schema["item_features"]):
-            if feature.dtype == "category":
-                layer_name = f"item_{feature.name}_embedding"
-                f_layer = CategoricalLayer(
-                    name=layer_name,
-                    n_unique_values=feature.unique_value_count,
-                    dimensions=feature_embedding_size,
-                    idx=feature_idx,
-                )
-                self.item_features.append(f_layer)
-                self.item_feature_dimension += feature_embedding_size
-            elif feature.dtype == "int64":
-                layer_name = f"item_{feature.name}_numerical"
-                f_layer = NumericalLayer(
-                    name=layer_name,
-                    idx=feature_idx,
-                )
-                self.item_features.append(f_layer)
-                self.item_feature_dimension += 1
+        self.item_features, self.item_feature_dimension = schema_to_featureModuleList(
+            data_schema["item_features"], feature_embedding_size
+        )
 
         # embedding for BERT, sum of positional, segment, token embeddings
         self.embedding = BERTEmbedding(
